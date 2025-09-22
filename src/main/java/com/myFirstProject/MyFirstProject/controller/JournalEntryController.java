6971cf8905e490d3b26874a778fa6cd8e3a680d5
itsrohanhere <rohankha332@gmail.com>
2025-09-22 16:21:32 +0530

    Initial commit

---
+++ src/main/java/com/myFirstProject/MyFirstProject/controller/JournalEntryController.java
@@ -0,0 +1,9 @@
+package com.myFirstProject.MyFirstProject.controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JournalEntryController {
+
+
+
+    }
